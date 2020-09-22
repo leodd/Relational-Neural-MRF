933b5c2572cec03e523a36dbae8cc76dbb081570
leodd <410528518@qq.com>
2020-09-22 12:08:45 -0500

    update unit test

--- demo/unit_test/neural_net_mle_discrete_test.py
+++ demo/unit_test/neural_net_mle_discrete_test.py
@@ -1,39 +1,42 @@
 from NeuralNetPotential import GaussianNeuralNetPotential, TableNeuralNetPotential, LeakyReLU, ReLU, ELU, LinearLayer
 from Potentials import TableFunction
 from learning.NeuralPMLEHybrid import PMLE
-from utils import visualize_1d_potential
+from utils import visualize_1d_potential, visualize_2d_potential
 from Graph import *
 import numpy as np
 import seaborn as sns
 
 
-domain = Domain([0, 1, 2, 3], continuous=False)
+domain = Domain([0, 1], continuous=False)
 
-rv = RV(domain)
+rv1 = RV(domain)
+rv2 = RV(domain)
+
+choice = np.array([(0, 0), (0, 1), (1, 0), (1, 1)])
+temp = np.random.choice([0, 1, 2, 3], p=[0.1, 0.3, 0.2, 0.4], size=1000)
 
 data = {
-    rv: np.random.choice([0, 1, 2, 3], p=[0.1, 0.3, 0.2, 0.4], size=1000)
+    rv1: choice[temp, 0],
+    rv2: choice[temp, 1]
 }
 
-sns.distplot(data[rv])
-
 p = TableNeuralNetPotential(
-    layers=[LinearLayer(1, 64), ReLU(),
+    layers=[LinearLayer(2, 64), ReLU(),
             LinearLayer(64, 32), ReLU(),
             LinearLayer(32, 1)],
-    domains=[domain],
+    domains=[domain, domain],
     prior=TableFunction(
-        np.ones([domain.size]) / domain.size
+        np.ones([domain.size, domain.size]) / (domain.size * domain.size)
     )
 )
 
-f = F(p, nb=[rv])
+f = F(p, nb=[rv1, rv2])
 
-g = Graph({rv}, {f})
+g = Graph({rv1, rv2}, {f})
 
 def visualize(ps, t):
     if t % 200 == 0:
-        visualize_1d_potential(p, domain)
+        visualize_2d_potential(p, domain, domain)
 
 leaner = PMLE(g, [p], data)
 leaner.train(
