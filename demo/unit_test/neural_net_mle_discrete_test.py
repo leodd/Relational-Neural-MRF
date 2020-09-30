d4ff2906d71c464ddd673ec14667a9e45a474137
leodd <410528518@qq.com>
2020-09-30 14:17:28 -0500

    update unit test

--- demo/unit_test/neural_net_mle_discrete_test.py
+++ demo/unit_test/neural_net_mle_discrete_test.py
@@ -1,10 +1,12 @@
-from NeuralNetPotential import GaussianNeuralNetPotential, TableNeuralNetPotential, LeakyReLU, ReLU, ELU, LinearLayer, WSLinearLayer
+from NeuralNetPotential import GaussianNeuralNetPotential, TableNeuralNetPotential, LeakyReLU, ReLU, ELU, LinearLayer, WSLinearLayer, NormalizeLayer
 from Potentials import TableFunction
 from learning.NeuralPMLEHybrid import PMLE
 from utils import visualize_1d_potential, visualize_2d_potential
 from Graph import *
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 d1 = Domain([0, 1], continuous=False)
@@ -13,8 +15,8 @@ d2 = Domain([0, 1, 2, 3, 4], continuous=False)
 rv1 = RV(d1)
 rv2 = RV(d2)
 
-choice = np.array([(0, 0), (0, 1), (1, 0), (1, 1)])
-temp = np.random.choice([0, 1, 2, 3], p=[0.3, 0.2, 0.2, 0.3], size=1000)
+choice = np.array([(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4)])
+temp = np.random.choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], p=[0.05, 0.1, 0.2, 0.1, 0.05, 0.2, 0.1, 0.05, 0.1, 0.05], size=4000)
 
 data = {
     rv1: choice[temp, 0],
@@ -22,12 +24,16 @@ data = {
 }
 
 p = TableNeuralNetPotential(
-    layers=[WSLinearLayer([[0, 1]], 64), ReLU(),
-            LinearLayer(64, 32), ReLU(),
-            LinearLayer(32, 1)],
-    domains=[domain, domain],
+    layers=[
+        NormalizeLayer([(-1, 1)] * 2, domains=[d1, d2]),
+        # WSLinearLayer([[0, 1]], 64), ReLU(),
+        LinearLayer(2, 200), ReLU(),
+        LinearLayer(200, 100), ReLU(),
+        LinearLayer(100, 1)
+    ],
+    domains=[d1, d2],
     prior=TableFunction(
-        np.ones([domain.size, domain.size]) / (domain.size * domain.size)
+        np.ones([d1.size, d2.size]) / (d1.size * d2.size)
     )
 )
 
@@ -37,17 +43,31 @@ g = Graph({rv1, rv2}, {f})
 
 def visualize(ps, t):
     if t % 200 == 0:
-        visualize_2d_potential(p, domain, domain)
+        x1, x2 = np.meshgrid(d1.values, d2.values)
+        xs = np.array([x1, x2]).T.reshape(-1, 2)
+        ys = p.batch_call(xs).reshape(-1, 1)
+        ys = ys / np.sum(ys)
+        print(np.concatenate([xs, ys], axis=1))
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(x1, x2, ys)
+        ax.set_xlabel('x1')
+        ax.set_ylabel('x2')
+        ax.set_zlabel('value')
+        plt.show()
+
+        # visualize_2d_potential(p, d1, d2)
 
 leaner = PMLE(g, [p], data)
 leaner.train(
-    lr=0.0001,
-    alpha=0.999,
+    lr=0.001,
+    alpha=1.,
     regular=0.0,
     max_iter=5000,
     batch_iter=10,
     batch_size=300,
-    rvs_selection_size=1,
+    rvs_selection_size=2,
     sample_size=20,
     visualize=visualize
 )
